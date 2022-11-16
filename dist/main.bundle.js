b0a953e13e77170a91b521a9424a20f7fe2b4893
Qualiaz <qualiaz@protonmail.com>
2022-11-16 15:46:29 +0200

    init commit

---
+++ dist/main.bundle.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!********************!*\
+  !*** ./src/app.js ***!
+  \********************/
+
+console.log("gfd");
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5idW5kbGUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQWE7QUFDYiIsInNvdXJjZXMiOlsid2VicGFjazovL3dlYXRoZXItY2hlY2svLi9zcmMvYXBwLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIlwidXNlIHN0cmljdFwiO1xuY29uc29sZS5sb2coXCJnZmRcIik7XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
